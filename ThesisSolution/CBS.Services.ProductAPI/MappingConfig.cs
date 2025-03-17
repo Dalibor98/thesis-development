a5498f689075e75e3c42c83f6411e58d1943f033
Dalibor Naspalic <dalibor.naspalic@gmail.com>
2025-03-17 22:55:30 +0100

    Majority of the Product BE done,Program.cs configuration needed.

---
+++ ThesisSolution/CBS.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CBS.Services.ProductAPI.Models;
+using CBS.Services.ProductAPI.Models.DTO;
+
+namespace CBS.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
