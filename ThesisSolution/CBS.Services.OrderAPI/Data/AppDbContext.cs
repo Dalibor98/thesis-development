b889fcee9c826b1e7be4921b66b99b709cafd024
Dalibor Naspalic <dalibor.naspalic@gmail.com>
2025-03-18 23:05:54 +0100

    Added Order BE project,updated Db,MappingConfig,Dto's

---
+++ ThesisSolution/CBS.Services.OrderAPI/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CBS.Services.OrderAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CBS.Services.OrderAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+
+    }
+}
---
+++ ThesisSolution/CBS.Services.OrderAPI/Program.cs
@@ -0,0 +1,40 @@
+using AutoMapper;
+using CBS.Services.OrderAPI;
+using CBS.Services.OrderAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString"));
+});
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
