5aab92f276a2dc527a9bc7618b31ec98a40163fd
Dalibor Naspalic <dalibor.naspalic@gmail.com>
2025-03-25 18:38:32 +0100

    Updated a WP Program.cs / Injected UniversityId service

--- MainThesisSolution/MTS.Web/Program.cs
+++ MainThesisSolution/MTS.Web/Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddScoped<IBaseService, BaseService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IAdminService, AdminService>();
 builder.Services.AddScoped<IProfessorService, ProfessorService>();
+builder.Services.AddScoped<IUniversityIdService, UniversityIdService>();
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
